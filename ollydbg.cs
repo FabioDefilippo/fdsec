96c727925d4b95306f2253c79b365b961f99f733
Fabio Defilippo <138728583+FabioDefilippo@users.noreply.github.com>
2025-05-23 23:30:53 +0200

    Create ollydbg.cs

---
+++ ollydbg.cs
@@ -0,0 +1,15 @@
+using System.Threading;
+
+namespace ollydbg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
